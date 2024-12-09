fabb3fa3166d29654029b565bddef04aca85397f
nguyenthuy163 <thuyle.31231026554@st.ueh.edu.vn>
2024-12-09 23:59:29 +0700

    update program.cs homepage

--- Connect Four/Program.cs
+++ Connect Four/Program.cs
@@ -17,6 +17,7 @@ namespace Connect_Four
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new HomePage());
+            //hihi
         }
     }
 }
